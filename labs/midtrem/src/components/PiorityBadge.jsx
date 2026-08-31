429adbcbc990aae7c0aa6cb408a4905c8f736fcf
Njuntaya <ruizfeez99@gmail.com>
2026-08-31 14:04:02 +0700

    B4.1: PriorityBadge base

---
+++ labs/midtrem/src/components/PiorityBadge.jsx
@@ -0,0 +1,20 @@
+function PriorityBadge({ priority }) {
+  let label = 'ไม่ระบุ';
+  let className = 'priority-unknown';
+
+  if (priority === 'urgent') {
+    label = 'เร่งด่วน';
+    className = 'priority-urgent';
+  } else if (priority === 'normal') {
+    label = 'ปกติ';
+    className = 'priority-normal';
+  }
+
+  return (
+    <span className={`badge ${className}`}>
+      {label}
+    </span>
+  );
+}
+
+export default PriorityBadge;
--- labs/midtrem/src/components/RequestCard.jsx
+++ labs/midtrem/src/components/RequestCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import PriorityBadge from './PriorityBadge';
 
 function RequestCard({ request, onDeleteRequest, onMarkDone }) {
   return (
